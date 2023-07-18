b52c5fd40c26a15c84e3b75924ea42421168d4e2
Erik Johnson <erikjohnson06@gmail.com>
2023-07-18 05:47:20 -0400

    WIP - Building User components

---
+++ src/users/components/UsersList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <h2>No users found.</h2>
+            </div>
+        );
+    }
+    else {
+        return (
+            <ul className="users-list">
+                {props.items.map(user => (
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    locationCount={user.places}
+                    />
+                 ))}
+            </ul>
+        );
+    }
+};
+
+export default UsersList;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
+
+    const USERS = [
+        { id : 'u1', name : 'Erik Johnson', image: '', locationsCount: 3},
+    ];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
