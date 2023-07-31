a1d1230a5cbb62a0ce5157169504ea77973c0de5
Erik Johnson <erikjohnson06@gmail.com>
2023-07-31 05:37:42 -0400

    Adding header and navigation components. WIP

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import Users from './users/pages/Users';
 import NewLocation from './locations/pages/NewLocation';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
     return <Router>
-        <Routes>
-            <Route path="/" exact element={<Users />} />
-            <Route path="/locations/new" exact element={<NewLocation />} />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <MainNavigation />
+        <main>
+            <Routes>
+                <Route path="/" exact element={<Users />} />
+                <Route path="/locations/new" exact element={<NewLocation />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </main>
     </Router>;
 };
 
