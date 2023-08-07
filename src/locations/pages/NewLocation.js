ea17665ff4fac49e10f8a32c4cfa7ff64eb7653c
Erik Johnson <erikjohnson06@gmail.com>
2023-08-07 05:41:28 -0400

    Adding Input components and input validation logic

--- src/locations/pages/NewLocation.js
+++ src/locations/pages/NewLocation.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const NewLocation = () => {
-    return <h2>NewLocation Works!</h2>;
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewLocation.css';
+
+const NewLocation = props => {
+    return (
+            <form className="location-form">
+                <Input
+                    element="input"
+                    type="text"
+                    label="Title"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid title"
+                    />
+
+            </form>
+            );
 };
 
 export default NewLocation;
