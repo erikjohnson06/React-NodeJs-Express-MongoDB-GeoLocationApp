e36238d0340afa7a45a1bb3976594f3831adca3a
Erik Johnson <erikjohnson06@gmail.com>
2023-09-05 05:51:04 -0400

    Adjusting UpdateLocation page to fetch/post database from database

--- src/locations/pages/UpdateLocation.js
+++ src/locations/pages/UpdateLocation.js
@@ -1,49 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 import {
     VALIDATOR_REQUIRE,
     VALIDATOR_MINLENGTH
 } from '../../shared/util/validators';
+
 import { useForm } from '../../shared/hooks/form-hooks';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+
 import './LocationForm.css';
-
-const DUMMY_DATA = [
-    {
-        id: 'p1',
-        title: 'Test Location 1',
-        description: 'A really famous and amazing location!',
-        createdBy: 'u1',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        coordinates: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        }
-    },
-    {
-        id: 'p2',
-        title: 'Test Location 2',
-        description: 'A really famous and amazing location!',
-        createdBy: 'u2',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        coordinates: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        }
-    }
-
-];
 
 const UpdateLocation = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
+    const auth = useContext(AuthContext);
+    const { isLoading, hasError, sendRequest, clearError } = useHttpClient();
+    const [loadedLocation, setLoadedLocation] = useState();
+
     const locationId = useParams().locId;
+
+    const navigate = useNavigate();
 
     const [formState, inputHandler, setFormData] = useForm({
         title: {
@@ -60,79 +44,115 @@ const UpdateLocation = () => {
             }
     }, true);
 
-    const location = DUMMY_DATA.find(l => l.id === locationId);
-
     useEffect(() => {
 
-        if (location){
-            setFormData({
-                title: {
-                    value: location.title,
-                    isValid: true
-                },
-                description: {
-                    value: location.description,
-                    isValid: true
-                },
-                address: {
-                    value: location.address,
-                    isValid: true
+        const fetchLocation = async () => {
+
+            try {
+                const response = await sendRequest(
+                        `http://localhost:5000/api/locations/${locationId}`
+                    );
+
+                setLoadedLocation(response.location);
+
+                setFormData({
+                    title: {
+                        value: response.location.title,
+                        isValid: true
+                    },
+                    description: {
+                        value: response.location.description,
+                        isValid: true
+                    },
+                    address: {
+                        value: response.location.address,
+                        isValid: true
+                        }
+                }, true);
+            }
+            catch (e){
+                console.log(e);
+            }
+        };
+
+        fetchLocation();
+
+    }, [sendRequest, locationId, setFormData]);
+
+    const locationUpdateSubmitHandler = async event => {
+
+        event.preventDefault();
+
+        try {
+
+            await sendRequest(
+                    `http://localhost:5000/api/locations/${locationId}`,
+                    'PATCH',
+                    JSON.stringify({
+                        title: formState.inputs.title.value,
+                        description: formState.inputs.description.value,
+                        address: formState.inputs.address.value,
+                        updatedBy: auth.userId
+                    }),
+                    {
+                        'Content-Type': 'application/json'
                     }
-            }, true);
+                );
+
+            //Redirect to user locations
+            navigate('/' + auth.userId + '/locations');
+        } catch (e) {
+            console.log(e);
         }
 
-        setIsLoading(false);
-
-    }, [setFormData, location]);
-
-    const locationUpdateSubmitHandler = event => {
-        event.preventDefault();
-        console.log(formState.inputs);
     };
 
-    if (!location) {
+    if (isLoading){
+        return <div className="center"><LoadingSpinner /></div>;
+    }
+    else if (!loadedLocation && !hasError) {
         return <div className="center"><Card><h2>Unable to find this location</h2></Card></div>;
     }
 
-    if (isLoading){
-        return <div className="center"><Card><h2>Loading...</h2></Card></div>;
-    }
-
     return (
-            <form className="location-form" onSubmit={locationUpdateSubmitHandler}>
-                <Input
-                    id="title"
-                    element="input"
-                    type="text"
-                    label="Title"
-                    validators={[VALIDATOR_REQUIRE()]}
-                    errorText="Please enter a valid title"
-                    onInput={inputHandler}
-                    initialValue={formState.inputs.title.value}
-                    initialValid={formState.inputs.title.isValid}
-                    />
-                <Input
-                    id="description"
-                    element="textarea"
-                    label="Description"
-                    validators={[VALIDATOR_MINLENGTH(5)]}
-                    errorText="Please enter a valid description (minimum of 5 characters)"
-                    onInput={inputHandler}
-                    initialValue={formState.inputs.description.value}
-                    initialValid={formState.inputs.description.isValid}
-                    />
-                <Input
-                    id="address"
-                    element="input"
-                    label="Address"
-                    validators={[VALIDATOR_REQUIRE()]}
-                    errorText="Please enter a valid address"
-                    onInput={inputHandler}
-                    initialValue={formState.inputs.address.value}
-                    initialValid={formState.inputs.address.isValid}
-                    />
-                <Button type="submit" disabled={!formState.isValid}>Update Location</Button>
-            </form>
+            <React.Fragment>
+                <ErrorModal error={hasError} onClear={clearError} />
+                {!isLoading && loadedLocation && (
+                <form className="location-form" onSubmit={locationUpdateSubmitHandler}>
+                    <Input
+                        id="title"
+                        element="input"
+                        type="text"
+                        label="Title"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a valid title"
+                        onInput={inputHandler}
+                        initialValue={loadedLocation.title}
+                        initialValid={true}
+                        />
+                    <Input
+                        id="description"
+                        element="textarea"
+                        label="Description"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter a valid description (minimum of 5 characters)"
+                        onInput={inputHandler}
+                        initialValue={loadedLocation.description}
+                        initialValid={true}
+                        />
+                    <Input
+                        id="address"
+                        element="input"
+                        label="Address"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a valid address"
+                        onInput={inputHandler}
+                        initialValue={loadedLocation.address}
+                        initialValid={true}
+                        />
+                    <Button type="submit" disabled={!formState.isValid}>Update Location</Button>
+                </form>)}
+            </React.Fragment>
         );
 };
 
