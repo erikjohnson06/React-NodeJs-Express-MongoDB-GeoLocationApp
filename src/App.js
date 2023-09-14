8b85b42ae28b05dd3a8be81f4cc883550ae1aab3
Erik Johnson <erikjohnson06@gmail.com>
2023-09-14 05:46:25 -0400

    Adding login state and token expiration management to front end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Auth from './users/pages/Auth';
@@ -9,20 +9,61 @@ import UserLocations from './locations/pages/UserLocations';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
 
     const [token, setToken] = useState(null);
-    const [userId, setUserId] = useState(false);
+    const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
+    const [userId, setUserId] = useState(null);
 
-    const login = useCallback((id, token) => {
+    const login = useCallback((id, token, expirationDate) => {
+
+        const tokenExpiration = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); //1 Hour
+
         setToken(token);
         setUserId(id);
+        setTokenExpirationDate(tokenExpiration);
+
+        localStorage.setItem('userData',
+        JSON.stringify({
+            userId: id,
+            token: token,
+            expiration : tokenExpiration.toISOString()
+        }));
     }, []);
 
     const logout = useCallback(() => {
         setToken(null);
         setUserId(null);
+        setTokenExpirationDate(null);
+        localStorage.removeItem('userData');
     }, []);
+
+    useEffect(() => {
+        if (token && tokenExpirationDate){
+
+            const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+            console.log("remainingTime: ", remainingTime);
+
+            logoutTimer = setTimeout(logout, remainingTime);
+        }
+        else {
+            clearTimeout(logoutTimer);
+        }
+    }, [token, logout, tokenExpirationDate]);
+
+    //Auto-login if already authenticated
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData'));
+
+        const expirationDate = (storedData.expiration ? new Date(storedData.expiration) : new Date());
+        const currDate = new Date();
+
+        if (storedData && storedData.token && expirationDate > currDate){
+            login(storedData.userId, storedData.token, expirationDate);
+        }
+    }, [login]);
 
     let routes;
 
